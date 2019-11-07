340715e8e65192b6447d4518ba8405a1ccdf48a7
yuansmin <yuansmin1@163.com>
2019-11-07 22:29:44 +0800

    optimize swapped check

--- go/sort/bubblesort/sort.go
+++ go/sort/bubblesort/sort.go
@@ -2,18 +2,17 @@ package sort
 
 func Sort(data []int) []int {
 	length := len(data)
-	swaped := true
 	for j := length; j > 0; j-- {
-		// the array is sorted if no swaped happened in pre bubble loop
-		if !swaped {
-			return data
-		}
-		swaped = false
+		// the array is sorted if no swaped happened in one bubble loop
+		swaped := false
 		for i := 1; i < j; i++ {
 			if data[i-1] > data[i] {
 				data[i-1], data[i] = data[i], data[i-1]
 				swaped = true
 			}
+		}
+		if !swaped {
+			return data
 		}
 	}
 	return data
